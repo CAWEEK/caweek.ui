682e3168d045cd538dc273d3ff7d15c6368c0faa
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2022-03-03 22:45:50 +0900

    feat: add onclick action and click action/ pointer when disabled

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import Button from '.';
 
 export default {
@@ -33,75 +34,79 @@ export const button = () => {
 	const size = select('size', ['tiny', 'small', 'medium', 'large'], 'medium');
 	const type = select('type', ['prime', 'sub', 'cancel'], 'prime');
 	const isDisabled = boolean('isDisabled', false);
+	const clickAction = action('clicked');
 
 	return (
-		<Button size={size} type={type} isDisabled={isDisabled}>
+		<Button size={size} type={type} isDisabled={isDisabled} onClick={clickAction}>
 			{label}
 		</Button>
 	);
 };
 
-export const ButtonList = () => (
-	<ButtonWrapper>
-		<div className="description">Tiny</div>
-		<InnerWrapper>
-			<Button size="tiny" type="prime">
-				PRIME
-			</Button>
-			<Button size="tiny" type="sub">
-				SUB
-			</Button>
-			<Button size="tiny" type="cancel">
-				CANCEL
-			</Button>
-			<Button size="tiny" isDisabled>
-				DISABLED
-			</Button>
-		</InnerWrapper>
-		<div className="description">Small</div>
-		<InnerWrapper>
-			<Button size="small" type="prime">
-				PRIME
-			</Button>
-			<Button size="small" type="sub">
-				SUB
-			</Button>
-			<Button size="small" type="cancel">
-				CANCEL
-			</Button>
-			<Button size="small" isDisabled>
-				DISABLED
-			</Button>
-		</InnerWrapper>
-		<div className="description">Medium</div>
-		<InnerWrapper>
-			<Button size="medium" type="prime">
-				PRIME
-			</Button>
-			<Button size="medium" type="sub">
-				SUB
-			</Button>
-			<Button size="medium" type="cancel">
-				CANCEL
-			</Button>
-			<Button size="medium" isDisabled>
-				DISABLED
-			</Button>
-		</InnerWrapper>
-		<div className="description">Large</div>
-		<InnerWrapper>
-			<Button size="large" type="prime">
-				PRIME
-			</Button>
-			<Button size="large" type="sub">
-				SUB
-			</Button>
-			<Button size="large" type="cancel">
-				CANCEL
-			</Button>
-			<Button size="large" isDisabled>
-				DISABLED
-			</Button>
-		</InnerWrapper>
-	</ButtonWrapper>
-);
+export const ButtonList = () => {
+	const clickAction = action('clicked');
+	return (
+		<ButtonWrapper>
+			<div className="description">Tiny</div>
+			<InnerWrapper>
+				<Button size="tiny" type="prime" onClick={clickAction}>
+					PRIME
+				</Button>
+				<Button size="tiny" type="sub" onClick={clickAction}>
+					SUB
+				</Button>
+				<Button size="tiny" type="cancel" onClick={clickAction}>
+					CANCEL
+				</Button>
+				<Button size="tiny" isDisabled onClick={clickAction}>
+					DISABLED
+				</Button>
+			</InnerWrapper>
+			<div className="description">Small</div>
+			<InnerWrapper>
+				<Button size="small" type="prime" onClick={clickAction}>
+					PRIME
+				</Button>
+				<Button size="small" type="sub" onClick={clickAction}>
+					SUB
+				</Button>
+				<Button size="small" type="cancel" onClick={clickAction}>
+					CANCEL
+				</Button>
+				<Button size="small" isDisabled onClick={clickAction}>
+					DISABLED
+				</Button>
+			</InnerWrapper>
+			<div className="description">Medium</div>
+			<InnerWrapper>
+				<Button size="medium" type="prime" onClick={clickAction}>
+					PRIME
+				</Button>
+				<Button size="medium" type="sub" onClick={clickAction}>
+					SUB
+				</Button>
+				<Button size="medium" type="cancel" onClick={clickAction}>
+					CANCEL
+				</Button>
+				<Button size="medium" isDisabled onClick={clickAction}>
+					DISABLED
+				</Button>
+			</InnerWrapper>
+			<div className="description">Large</div>
+			<InnerWrapper>
+				<Button size="large" type="prime" onClick={clickAction}>
+					PRIME
+				</Button>
+				<Button size="large" type="sub" onClick={clickAction}>
+					SUB
+				</Button>
+				<Button size="large" type="cancel" onClick={clickAction}>
+					CANCEL
+				</Button>
+				<Button size="large" isDisabled onClick={clickAction}>
+					DISABLED
+				</Button>
+			</InnerWrapper>
+		</ButtonWrapper>
+	);
+};
