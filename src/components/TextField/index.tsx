665a3b923ce797b33a662fbe9f05ca6205b4da59
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2022-05-10 20:45:10 +0900

    feat: add full props

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -9,11 +9,12 @@ type TextFieldProps = {
 	value?: string;
 	onChange?: ChangeEventHandler<HTMLInputElement>;
 	placeholder?: string;
+	full?: boolean;
 };
 
-const BaseTextField = styled.input`
-	width: 280px;
-	height: 48px;
+const BaseTextField = styled.input<{ full?: boolean }>`
+	width: ${({ full }) => (full ? '100%' : '280px')};
+	height: ${({ full }) => (full ? '64px' : '48px')};
 	border-radius: 12px;
 	background-color: ${({ theme: { colors } }) => colors.blueGrey10};
 	box-sizing: border-box;
@@ -22,8 +23,8 @@ const BaseTextField = styled.input`
 	display: flex;
 	align-items: center;
 
-	font-size: ${({ theme: { fontSizes } }) => fontSizes.subhead2};
-	font-weight: ${({ theme: { fontWeights } }) => fontWeights.subhead2};
+	font-size: ${({ theme: { fontSizes }, full }) => (full ? fontSizes.headline1 : fontSizes.subhead2)};
+	font-weight: ${({ theme: { fontWeights }, full }) => (full ? fontWeights.headline1 : fontWeights.subhead2)};
 	color: ${({ theme: { colors } }) => colors.grey20};
 
 	::placeholder {
@@ -37,10 +38,10 @@ const BaseTextField = styled.input`
 	}
 `;
 
-function TextField({ value, onChange, placeholder }: TextFieldProps) {
+function TextField({ value, onChange, placeholder, full }: TextFieldProps) {
 	return (
 		<ThemeProvider theme={theme}>
-			<BaseTextField value={value} onChange={onChange} placeholder={placeholder} />
+			<BaseTextField value={value} onChange={onChange} placeholder={placeholder} full={full} />
 		</ThemeProvider>
 	);
 }
@@ -49,6 +50,7 @@ TextField.defaultProps = {
 	value: undefined,
 	onChange: undefined,
 	placeholder: undefined,
+	full: undefined,
 };
 
 export default TextField;
